974caf7c8548ff75374fe22dea409d4e69ed223e
SuRen <811635037@QQ.com>
2018-11-27 14:26:20 +0800

    safdgsda

--- app/src/main/java/com/example/windows/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/windows/gitdemo/MainActivity.java
@@ -9,5 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //分类
+        //购物车
+        //撒可富屁哦我BFXV
+
     }
 }
